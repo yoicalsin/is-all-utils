d67312cb1fbffef3542397af0387dd9a33e375df
Yoni Calsin <yonicb20@gmail.com>
2020-04-22 14:54:50 -0500

    style: 💄 Aggregated is functions !

---
+++ index.ts
@@ -0,0 +1,13 @@
+export const is = (type?: any) => (value: any) =>
+  toString.call(value) === toString.call(type);
+
+export const isObject = is({});
+export const isArray = is([]);
+export const isNumber = is(7);
+export const isString = is("");
+export const isClass = is(class {});
+export const isRegex = is(/(?:Yoni\s*Calsin)+/gi);
+export const isBoolean = is(true);
+export const isUndefined = is();
+export const isNull = is(null);
+export const isFunction = is(is);
