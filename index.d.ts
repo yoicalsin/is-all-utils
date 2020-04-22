37f0eb469ad919921884b407dddd246cbc15ff62
Yoni Calsin <yonicb20@gmail.com>
2020-04-22 15:07:34 -0500

    style: 💄 Created types for this typescript !

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export declare const is: (type?: any) => (value: any) => boolean;
+export declare const isObject: (value: any) => boolean;
+export declare const isArray: (value: any) => boolean;
+export declare const isNumber: (value: any) => boolean;
+export declare const isString: (value: any) => boolean;
+export declare const isClass: (value: any) => boolean;
+export declare const isRegex: (value: any) => boolean;
+export declare const isBoolean: (value: any) => boolean;
+export declare const isUndefined: (value: any) => boolean;
+export declare const isNull: (value: any) => boolean;
+export declare const isFunction: (value: any) => boolean;
