e586de8df993ee7ab71a7ca3c77564c4cdf392d8
Yoni Calsin <yonicb20@gmail.com>
2020-04-22 20:29:00 -0500

    fix: 🐛 Removed isPrimitive function, and !

--- index.ts
+++ index.ts
@@ -1,33 +1,32 @@
 const is = (type?: any, fn?: Function) => (value: any) => {
-  const isAfter = toString.call(value) === toString.call(type);
-  if (type == "none" && fn) return fn(value);
-  if (fn) return isAfter && fn(value);
-  return isAfter;
+    const iscall = toString.call(value) === toString.call(type);
+    if (type == 'fn' && fn) return fn(value);
+    if (fn) return iscall && fn(value);
+    return iscall;
 };
+// Number
+export const isNumber = is(7);
+export const isInteger = is(7, Number.isInteger);
 
+// Object
 export const isObject = is({});
 export const isArray = is([]);
-export const isNumber = is(7);
-export const isNaN = is(NaN);
-export const isString = is("");
-export const isClass = is(class {});
+export const isString = is('');
 export const isRegex = is(/7/gi);
 export const isBoolean = is(true);
+
+// Null
 export const isUndefined = is();
 export const isNull = is(null);
+
+// Function
 export const isFunction = is(is);
+export const isClass = is(class {});
+export const isSymbol = is(Symbol());
+
+// Error
 export const isError = is(new Error());
 export const isDate = is(new Date());
-export const isInteger = is(7, Number.isInteger);
-export const isSymbol = is(Symbol());
-export const isPrimitive = is(
-  "none",
-  (value: any) =>
-    isNumber(value) ||
-    isBoolean(true) ||
-    isString(value) ||
-    isSymbol(value) ||
-    isUndefined(value)
-);
 
-export { is };
+// Promise
+export const isPromise = is(new Promise(() => {}));
