cc3cc110d01cb9d7da04496f9abbfd2291c9bc30
Yoni Calsin <yonicb20@gmail.com>
2020-05-03 21:23:13 -0500

    perf: ⚡️ Builded source !

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 const is = (type) => (value) => {
-    if (type && type.name != 'is' && isFunction(type)) return type(value);
+    if (type && type.name != 'is' && typeof type === 'function') {
+        return type(value);
+    }
     return toString.call(value) === toString.call(type);
 };
 exports.is = is;
 exports.isNumber = is(7);
 exports.isInteger = is(Number.isInteger);
 exports.isFloat = is((v) => /(?:\.)+/.test(parseFloat(v)));
+exports.isObj = is((v) => typeof v === 'object');
 exports.isObject = is({});
 exports.isArray = is([]);
 exports.isString = is('');
 exports.isRegex = is(/7/gi);
 exports.isBoolean = is(true);
+exports.isBool = is((v) => !!v);
 exports.isUndefined = is();
 exports.isNull = is(null);
+exports.isNil = is((v) => isUndefined(v) || isNull(v));
 exports.isFunction = is(is);
 exports.isSymbol = is(Symbol());
 exports.isError = is(new Error());
