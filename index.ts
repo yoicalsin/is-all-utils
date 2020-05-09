feffda9bb182c27194aafaf5ed4c4649a539b44c
Yoni Calsin <yonicb20@gmail.com>
2020-05-09 15:30:03 -0500

    perf: ⚡️ Aggregated more functions !

--- index.ts
+++ index.ts
@@ -7,16 +7,22 @@ const is = <T = any>(type?: any) => (value: T): boolean => {
 
 // Number
 export const isNumber = is(7);
+export const isNumberFn = is((v) => v === Number);
 export const isInteger = is<number>(Number.isInteger);
 export const isFloat = is<number>((v) => /(?:\.)+/.test(parseFloat(v) as any));
 
 // Object
 export const isObj = is((v: any) => typeof v === 'object');
 export const isObject = is({});
+export const isObjectFn = is((v) => v === Object);
 export const isArray = is([]);
+export const isArrayFn = is((v) => v === Array);
 export const isString = is('');
-export const isRegex = is(/7/gi);
+export const isStringFn = is((v) => v === String);
+export const isRegExp = is(/7/gi);
+export const isRegExpFn = is((v) => v === RegExp);
 export const isBoolean = is(true);
+export const isBooleanFn = is((v) => v === Boolean);
 export const isBool = is((v) => isBoolean(v) || v == 0 || v == 1);
 
 // Null
@@ -29,6 +35,7 @@ export const isFunction = is(is);
 export const isAsyncFunction = is(async () => {});
 export const isFn = is((v: any) => typeof v === 'function');
 export const isSymbol = is(Symbol());
+export const isSymbolFn = is((v) => v === Symbol);
 
 // Class
 export const isClass = is(
@@ -37,9 +44,12 @@ export const isClass = is(
 
 // Error, Date
 export const isError = is(new Error());
+export const isErrorFn = is((v) => v === Error);
 export const isDate = is(new Date());
+export const isDateFn = is((v) => v === Date);
 
 // Promise
 export const isPromise = is(new Promise(() => {}));
+export const isPromiseFn = is((v) => v === Promise);
 
 export { is };
