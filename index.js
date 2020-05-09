809cc3d1b98cbe5f706b9bf135f1f55ec5f605cd
Yoni Calsin <yonicb20@gmail.com>
2020-05-09 14:47:15 -0500

    ci: 🎡 Builded to es2017 !

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 const is = (type) => (value) => {
-    if (type && type.name != 'is' && typeof type === 'function') {
+    if (toString.call(type) === '[object Function]' && type.name != 'is') {
         return type(value);
     }
     return toString.call(value) === toString.call(type);
@@ -16,12 +16,19 @@ exports.isArray = is([]);
 exports.isString = is('');
 exports.isRegex = is(/7/gi);
 exports.isBoolean = is(true);
-exports.isBool = is((v) => isBoolean(v) || v == 0 || v == 1);
+exports.isBool = is((v) => exports.isBoolean(v) || v == 0 || v == 1);
 exports.isUndefined = is();
 exports.isNull = is(null);
-exports.isNil = is((v) => isUndefined(v) || isNull(v));
+exports.isNil = is((v) => exports.isUndefined(v) || exports.isNull(v));
 exports.isFunction = is(is);
+exports.isAsyncFunction = is(async () => {});
+exports.isFn = is((v) => typeof v === 'function');
 exports.isSymbol = is(Symbol());
+exports.isClass = is(
+    (v) =>
+        exports.isObject((v || {}).prototype) &&
+        exports.isFn(v.prototype.constructor),
+);
 exports.isError = is(new Error());
 exports.isDate = is(new Date());
 exports.isPromise = is(new Promise(() => {}));
