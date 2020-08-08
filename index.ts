1cc3e834e90e65e94f390a431e7e0ee530d865bd
Yoni Calsin <yonicb20@gmail.com>
2020-08-08 17:10:04 -0500

    fix: 🐛 removed the function validate

--- index.ts
+++ index.ts
@@ -7,32 +7,22 @@ const is = <T = any>(type?: any) => (value: T): boolean => {
 
 // Number
 export const isNumber = is(7);
-export const isNumberFn = is((v) => v === Number);
 export const isInteger = is<number>(Number.isInteger);
 export const isFloat = is<number>((v) => /(?:\.)+/.test(v));
 
 // Object
 export const isObj = is((v: any) => typeof v === 'object');
 export const isObject = is({});
-export const isObjectFn = is((v) => v === Object);
 export const isArray = is([]);
-export const isArrayFn = is((v) => v === Array);
 export const isString = is('');
-export const isStringFn = is((v) => v === String);
 export const isRegExp = is(/7/gi);
-export const isRegExpFn = is((v) => v === RegExp);
 export const isBoolean = is(true);
-export const isBooleanFn = is((v) => v === Boolean);
 export const isBool = is((v) => isBoolean(v) || v == 0 || v == 1);
 
 export const isMap = is(new Map());
-export const isMapFn = is((v) => v === Map);
 export const isWeakMap = is(new WeakMap());
-export const isWeakMapFn = is((v) => v === WeakMap);
 export const isSet = is(new Set());
-export const isSetFn = is((v) => v === Set);
 export const isWeakSet = is(new WeakSet());
-export const isWeakSetFn = is((v) => v === WeakSet);
 
 // Null
 export const isUndefined = is();
@@ -44,7 +34,6 @@ export const isFunction = is(is);
 export const isAsyncFunction = is(async () => {});
 export const isFn = is((v: any) => typeof v === 'function');
 export const isSymbol = is(Symbol());
-export const isSymbolFn = is((v) => v === Symbol);
 
 // Class
 export const isClass = is(
@@ -53,12 +42,9 @@ export const isClass = is(
 
 // Error, Date
 export const isError = is(new Error());
-export const isErrorFn = is((v) => v === Error);
 export const isDate = is(new Date());
-export const isDateFn = is((v) => v === Date);
 
 // Promise
 export const isPromise = is(new Promise(() => {}));
-export const isPromiseFn = is((v) => v === Promise);
 
 export { is };
